a501fea85cbffdf50f20725c0cf87899f0689c28
Rohan Puranik <59736919+Rohan-3@users.noreply.github.com>
2022-06-24 12:02:25 +0530

    Section 3 completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-
 import './App.css';
+import Expenses from './Components/Expenses';
 
 function App() {
+
+    const expenses = [
+      { id: "e1",
+       title: "Cars insurance",
+       amount: "1000/-",
+       date: new Date(2022, 5, 23)
+      },
+      { id: "e2",
+       title: "Home insurance",
+       amount: "2000/-",
+       date: new Date(2022, 6, 23)
+      },
+      { id: "e3",
+       title: "Bike insurance",
+       amount: "500/-",
+       date: new Date(2022, 7, 23)
+      },
+      { id: "e4",
+       title: "Life insurance",
+       amount: "2000/-",
+       date: new Date(2022, 8, 23)
+      }
+    ]
+
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <h1>Welcome Boi</h1>
+     <Expenses items ={expenses}/>
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
 
-function ExpenseItem() {
-  return (
-    <div>ExpenseItem</div>
+function ExpenseItem(props) {
+
+  // // const ExpenseDate = new Date(2022, 6, 26).toISOString();
+  // // const ExpenseTitle = "Car insurance";
+  // // const ExpenseAmount = "1000/-"
+  // const month = props.date.toLocaleString('en-US', { month: 'long'});
+  // const year = props.date.getFullYear();
+  // const day = props.date.toLocaleString('en-US', { day: '2-digit'});
+
+
+    return (
+    <div className='expense-item'>
+    <ExpenseDate date= {props.date}/>
+    <div className='expense-item__description'>
+    <h2>{props.title}</h2>
+    <div className='expense-item__price'>INR {props.amount}</div>
+    </div>
+    <button>ClickChange</button>
+    </div>
+    
   )
 }
 
